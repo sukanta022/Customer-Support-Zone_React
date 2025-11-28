18c84ab7921417a03e15ad91ce76ee958b417e77
sukanta022 <sukantacb20@gmail.com>
2025-11-28 11:06:08 +0600

    added all tickets in UI

---
+++ src/Components/Ticket.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Calendar } from 'lucide-react';
+const Ticket = ({ticket}) => {
+    console.log(ticket)
+    return (
+        <div className=' bg-[#FFFFFF] shadow p-3 space-y-3'>
+            <div className='flex justify-between items-center'>
+                <p className='text-[18px] '>{ticket.title}</p>
+                
+                <div className={`${ticket.status==="In- Progress" ? "bg-[#F8F3B9]" : "bg-[#B9F8CF]"} py-1 px-3 bg-[#B9F8CF] rounded-2xl flex justify-center items-center gap-2`}>
+                    <p className={`${ticket.status==="In- Progress" ? "bg-[#FEBB0C]": "bg-[#02A53B]"} w-4 h-4 rounded-full`}></p>
+                    <p> {ticket.status}</p>
+                </div>
+            </div>
+
+            <p className='text-[16px] text-[#627382]'>{ticket.description}</p>
+
+            <div className='flex justify-between items-center'>
+                <div className='text-[14px] flex gap-3'>
+                    <p className=' text-[#627382]'>{ticket.id}</p>
+                    <p className={`${ticket.priority==="HIGH PRIORITY" && "text-[#F83044]"} ${ticket.priority==="MEDIUM PRIORITY" && "text-[#FEBB0C]"} ${ticket.priority==="LOW PRIORITY" && "text-[#02A53B]"}`}>{ticket.priority}</p>
+                </div>
+
+                <div className='text-[14px] text-[#627382] flex gap-3'>
+                    <p>{ticket.customer}</p>
+                    <Calendar className='w-5 h-5' />
+                    <p>{ticket.createdAt}</p>
+                </div>
+                            
+            </div>
+        </div>
+    );
+};
+
+export default Ticket;
--- src/Components/TicketContainer.jsx
+++ src/Components/TicketContainer.jsx
@@ -1,12 +1,25 @@
 import React, { use } from 'react';
 import State from './State';
+import Ticket from './Ticket';
 
 const TicketContainer = ({ticketPromise}) => {
     const allTickets = use(ticketPromise)
-    console.log(allTickets)
+    
     return (
         <div>
             <State></State>
+
+            <section className='w-11/12 mx-auto py-10 grid grid-cols-1 lg:grid-cols-12 gap-5'>
+                <div className='lg:col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                    {
+                        allTickets.map(ticket => <Ticket ticket={ticket}></Ticket>)
+                    }
+                </div>
+
+                <div className='lg:col-span-4 space-y-5 '>
+                    dfuh
+                </div>
+            </section>
         </div>
     );
 };
